bd3ada0d73f64cd06dfdd6651a5b5c3c58436378
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2023-10-30 15:21:04 -0400

    make friendly for both cross and native compiling

--- cross.sh
+++ cross.sh
@@ -1,1 +1,7 @@
+echo '[target.x86_64-unknown-linux-gnu]
+linker = "x86_64-unknown-linux-gnu-gcc"' > .cargo/config
+
 TARGET_CC="x86_64-unknown-linux-gnu-gcc" cargo build --target x86_64-unknown-linux-gnu
+
+rm .cargo/config
+
